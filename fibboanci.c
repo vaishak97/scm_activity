22c9999793618c7aa8b0124f0e8c4e0036cf2215
vaishak97 <57167262+vaishak97@users.noreply.github.com>
2019-10-30 12:14:28 +0530

    Add files via upload

--- fibboanci.c
+++ fibboanci.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 int main()
 {
-    int i, n, t1 = 0, t2 = 1, nextTerm;
+    int i, n, t1 = 0, t2 = 1;
     printf("Enter the number of terms: ");
     scanf("%d", &n);
     printf("Fibonacci Series: ");
     for (i = 1; i <= n; ++i)
     {
         printf("%d, ", t1);
-        nextTerm = t1 + t2;
+        int nextTerm = t1 + t2;
         t1 = t2;
         t2 = nextTerm;
     }
